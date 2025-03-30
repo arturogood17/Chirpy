edce76b5b1592524fc7d2f41d2ec28bca4bec8f9
arturogood17 <arturo_good17@hotmail.com>
2025-03-30 03:09:55 +0200

    Handler Readiness and handle func

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	const port = "8080"
 	mux := http.NewServeMux()
-
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 	server := http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: mux,
 	}
-	mux.Handle("/", http.FileServer(http.Dir("./")))
-	server.ListenAndServe()
+	fmt.Printf("Serving files from on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(http.StatusOK)
+	res.Write([]byte("OK"))
 }
