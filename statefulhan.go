fbb78ec9a11d332686002426912aa84e14cf5ca5
arturogood17 <arturo_good17@hotmail.com>
2025-03-31 23:24:36 +0200

    Chirps lenght testing req and res

--- statefulhan.go
+++ statefulhan.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -28,4 +29,47 @@ func (a *apiConfig) HandlerReset(res http.ResponseWriter, req *http.Request) {
 	a.hits.Store(0)
 	res.WriteHeader(http.StatusOK)
 	res.Write([]byte("Hits reset to 0"))
+}
+
+func (a *apiConfig) HandlerChirps(res http.ResponseWriter, req *http.Request) {
+	type JsonBody struct {
+		Body string `json:"body"`
+	}
+	type ErrorChirp struct {
+		Error string `json:"error"`
+	}
+	type ValidR struct {
+		Valid bool `json:"valid"`
+	}
+	decoder := json.NewDecoder(req.Body)
+	jb := JsonBody{}
+	if err := decoder.Decode(&jb); err != nil {
+		res.WriteHeader(500)
+		fmt.Printf("error al decodificar el request body: %v", err)
+		return
+	}
+	if len(jb.Body) > 140 || len(jb.Body) == 0 {
+		er := ErrorChirp{
+			Error: "Chirp is too long or is empty",
+		}
+		enc, err := json.Marshal(er)
+		if err != nil {
+			res.WriteHeader(500)
+			fmt.Printf("Something went wrong: %v", err)
+			return
+		}
+		res.WriteHeader(400)
+		res.Write(enc)
+		return
+	}
+	valid := ValidR{
+		Valid: true,
+	}
+	val, err := json.Marshal(valid)
+	if err != nil {
+		res.WriteHeader(500)
+		return
+	}
+	res.WriteHeader(200)
+	res.Write(val)
 }
