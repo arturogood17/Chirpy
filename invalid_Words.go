576ac619c1111f54bbf8396f8ba11db057587396
arturogood17 <arturo_good17@hotmail.com>
2025-04-02 00:52:24 +0200

    Chaking invalid words func()

---
+++ invalid_Words.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func WordValidation(chirp string) (string, error) {
+	invalidWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+
+	if len(chirp) == 0 {
+		return "", fmt.Errorf("chirp empty")
+	}
+	var chirpymod []string
+	s := strings.Fields(chirp)
+	for _, word := range s {
+		if _, ok := invalidWords[strings.ToLower(word)]; !ok {
+			chirpymod = append(chirpymod, word)
+		} else {
+			chirpymod = append(chirpymod, "****")
+		}
+	}
+	newchirpy := strings.Join(chirpymod, " ")
+	return newchirpy, nil
+}
