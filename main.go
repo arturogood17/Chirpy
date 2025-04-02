86d3a943c6f68ca482f9e9b7bc9c790966491ca2
arturogood17 <arturo_good17@hotmail.com>
2025-04-02 15:39:18 +0200

    added conection to db

--- main.go
+++ main.go
@@ -1,21 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/arturogood17/Chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
-	hits atomic.Int32
+	hits      atomic.Int32
+	dbQueries *database.Queries
 }
 
 func main() {
+	godotenv.Load(".env")
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbQueries := database.New(db)
 	const port = "8080"
 	mux := http.NewServeMux()
 	h := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 	a := apiConfig{}
+	a.dbQueries = dbQueries
 	mux.Handle("/app/", a.middlewareMetricsInc(h)) //Hay que quitar el app porque queremos servir
 	//los archivos que están en el dir actual
 	mux.HandleFunc("GET /api/healthz", handlerReadiness) //no tienes que crear un directorio para el path
