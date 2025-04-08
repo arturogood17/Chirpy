a91b82117ccc90fd385a63e1ddcac91b509b7229
arturogood17 <arturo_good17@hotmail.com>
2025-04-08 19:42:28 +0200

    Added query to update a user

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -13,5 +13,11 @@ VALUES(
 SELECT * from users
 WHERE email = $1;
 
+-- name: UpdateUser :one
+UPDATE users
+SET email = $1, hashed_password = $2
+WHERE id = $3
+RETURNING *;
+
 -- name: DeleteUsers :exec
 DELETE FROM users;
