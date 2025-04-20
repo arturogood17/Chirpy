bc35c9284ed9e3c2e75d966acdd248154592e6a1
arturogood17 <arturo_good17@hotmail.com>
2025-04-20 18:03:13 +0200

    RespondErrorWriter y responWithJson reworked. Added msg to ErrorWriter and everything now is marshalled in responWithJson

--- responses.go
+++ responses.go
@@ -6,24 +6,27 @@ import (
 	"net/http"
 )
 
-func respondErrorWriter(w http.ResponseWriter, error_code int, err error) {
-	type ErrorType struct {
+func respondErrorWriter(w http.ResponseWriter, error_code int, msg string, err error) { //Si tiene error, lo imprimes
+	type ErrorType struct { //Esta función no tiene ningún tipo de lógica de marshalling. Eso lo hace responWithJson
 		Error string `json:"error"`
 	}
-	errType := ErrorType{
-		Error: err.Error(),
-	}
-	data, err := json.Marshal(errType)
 	if err != nil {
-		responWithJson(w, http.StatusInternalServerError, []byte("Could not marshal error"))
-		return
+		log.Println(err)
 	}
-	responWithJson(w, error_code, data)
+	if error_code > 499 {
+		log.Printf("responding with 5XX error: %s\n", msg)
+	}
+	responWithJson(w, error_code, ErrorType{Error: msg})
 }
 
-func responWithJson(w http.ResponseWriter, code int, body []byte) {
-	w.WriteHeader(code)
-	if _, err := w.Write(body); err != nil {
-		log.Fatalf("Could respond - %v", err)
+func responWithJson(w http.ResponseWriter, code int, payload any) { //Esta función es la que hace el marshalling
+	w.Header().Set("Content-Type", "application/json") //Es importante establecer el Content-Type
+	data, err := json.Marshal(payload)                 //Marshal no necesita un struct. Solo convierte a JSON
+	if err != nil {                                    //El payload necesita ser any porque tiene que aceptar todo
+		log.Printf("error marshalling JSON - %v", err) //y marsheallearlo
+		w.WriteHeader(500)
+		return
 	}
+	w.WriteHeader(code)
+	w.Write(data)
 }
