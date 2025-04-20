466c2b6f135327070b450601fa3c0640b0f78cd2
arturogood17 <arturo_good17@hotmail.com>
2025-04-20 20:50:22 +0200

    Added way to eliminate profanities.

--- apiCode.go
+++ apiCode.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 func hValChirpy(w http.ResponseWriter, req *http.Request) {
@@ -15,13 +16,29 @@ func hValChirpy(w http.ResponseWriter, req *http.Request) {
 		respondErrorWriter(w, http.StatusInternalServerError, "Couldn't decode request body", err)
 		return
 	}
+	new_body := ProfanitiesCleaner(validC.Body)
 	const maxlenght = 140
-	if len(validC.Body) > maxlenght {
+	if len(new_body) > maxlenght {
 		respondErrorWriter(w, http.StatusBadRequest, "Chirp is too long", nil)
 		return
 	}
-	type validChirpy struct {
-		Valid bool `json:"valid"`
+	type CleanedB struct {
+		CleanedBody string `json:"cleaned_body"`
 	}
-	responWithJson(w, http.StatusOK, validChirpy{Valid: true})
+	responWithJson(w, http.StatusOK, CleanedB{CleanedBody: new_body})
+}
+
+func ProfanitiesCleaner(body string) string {
+	profanities := map[string]struct{}{
+		"kerfuffle": {}, //Los structs vacío se pueden definir así?
+		"sharbert":  {},
+		"fornax":    {},
+	}
+	body_split := strings.Fields(body)
+	for i, word := range body_split {
+		if _, ok := profanities[strings.ToLower(word)]; ok {
+			body_split[i] = "****"
+		}
+	}
+	return strings.Join(body_split, " ")
 }
