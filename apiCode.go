10d5928bd52426e6fd6c663693be618c4ddf412a
arturogood17 <arturo_good17@hotmail.com>
2025-04-20 18:01:09 +0200

    hValchirpy reworked. No marshalling logic here

--- apiCode.go
+++ apiCode.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"net/http"
 )
 
@@ -13,23 +12,16 @@ func hValChirpy(w http.ResponseWriter, req *http.Request) {
 	decoder := json.NewDecoder(req.Body)
 	var validC chirpyBody
 	if err := decoder.Decode(&validC); err != nil {
-		respondErrorWriter(w, http.StatusInternalServerError, err)
+		respondErrorWriter(w, http.StatusInternalServerError, "Couldn't decode request body", err)
 		return
 	}
-	if len(validC.Body) > 140 {
-		respondErrorWriter(w, http.StatusBadRequest, errors.New("Chirp is too long"))
+	const maxlenght = 140
+	if len(validC.Body) > maxlenght {
+		respondErrorWriter(w, http.StatusBadRequest, "Chirp is too long", nil)
 		return
 	}
 	type validChirpy struct {
 		Valid bool `json:"valid"`
 	}
-	GoodChirpy := validChirpy{
-		Valid: true,
-	}
-	ValidData, err := json.Marshal(GoodChirpy)
-	if err != nil {
-		respondErrorWriter(w, http.StatusInternalServerError, err)
-		return
-	}
-	responWithJson(w, http.StatusOK, ValidData)
+	responWithJson(w, http.StatusOK, validChirpy{Valid: true})
 }
