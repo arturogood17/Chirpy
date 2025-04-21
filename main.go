c676d228b4806a87bb8064c3b2b91570086b81bb
arturogood17 <arturo_good17@hotmail.com>
2025-04-22 01:07:50 +0200

    Added getenv, a db connection and created dbQueries. Imported postgres engine and database/sql, etc. Added Queries to apiconfig.

--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/arturogood17/Chirpy/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	serverHits atomic.Int32
+	Queries    *database.Queries
 }
 
 func main() {
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error open channel to db - %v", err)
+	}
+	dbQueries := database.New(db)
 	const port = "8080"
-	cfg := apiConfig{}
+	cfg := apiConfig{
+		Queries: dbQueries,
+	}
 	mux := http.NewServeMux()
 	srvr := http.Server{
 		Handler: mux,
@@ -23,7 +38,7 @@ func main() {
 	mux.HandleFunc("POST /admin/reset", cfg.handlerServerHitsReset)
 	mux.HandleFunc("GET /api/healthz", hReadiness)
 	mux.HandleFunc("POST /api/validate_chirp", hValChirpy)
-	err := srvr.ListenAndServe()
+	err = srvr.ListenAndServe()
 	if err != nil {
 		log.Fatalf("couldn't listen and serve from server - %v", err)
 	}
