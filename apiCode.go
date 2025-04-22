b9a4bc46ab141d3ecd26737ce1cc41004ed5a013
arturogood17 <arturo_good17@hotmail.com>
2025-04-22 22:43:21 +0200

    Created handler to create user, created user struct with json tags

--- apiCode.go
+++ apiCode.go
@@ -4,7 +4,17 @@ import (
 	"encoding/json"
 	"net/http"
 	"strings"
+	"time"
+
+	"github.com/google/uuid"
 )
+
+type User struct {
+	Id         uuid.UUID `json:"id"`
+	Created_at time.Time `json:"created_at"`
+	Updated_at time.Time `json:"updated_at"`
+	Email      string    `json:"email"`
+}
 
 func hValChirpy(w http.ResponseWriter, req *http.Request) {
 	type chirpyBody struct {
@@ -41,4 +51,28 @@ func ProfanitiesCleaner(body string) string {
 		}
 	}
 	return strings.Join(body_split, " ")
+}
+
+func (cfg *apiConfig) hUser(w http.ResponseWriter, req *http.Request) {
+	type UserEmail struct {
+		Email string `json:"email"`
+	}
+	decoder := json.NewDecoder(req.Body)
+	var userMail UserEmail
+	err := decoder.Decode(&userMail)
+	if err != nil {
+		respondErrorWriter(w, http.StatusInternalServerError, "Error trying to decode de request", err)
+		return
+	}
+	user, err := cfg.Queries.CreateUser(req.Context(), userMail.Email)
+	if err != nil {
+		respondErrorWriter(w, http.StatusInternalServerError, "Error trying to query the database to create user", err)
+		return
+	}
+	responWithJson(w, http.StatusCreated, User{
+		Id:         user.ID,
+		Created_at: user.CreatedAt,
+		Updated_at: user.UpdatedAt,
+		Email:      user.Email,
+	})
 }
