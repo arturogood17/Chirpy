ba56fe73fae8b14b19cde537b58a2bda247cf528
arturogood17 <arturo_good17@hotmail.com>
2025-04-24 00:06:39 +0200

    Go code generated by sqlc

--- internal/database/chirps.sql.go
+++ internal/database/chirps.sql.go
@@ -74,3 +74,21 @@ func (q *Queries) ListChirps(ctx context.Context) ([]Chirp, error) {
 	}
 	return items, nil
 }
+
+const singleChirp = `-- name: SingleChirp :one
+SELECT id, created_at, updated_at, body, user_id FROM chirps
+WHERE id = $1
+`
+
+func (q *Queries) SingleChirp(ctx context.Context, id uuid.UUID) (Chirp, error) {
+	row := q.db.QueryRowContext(ctx, singleChirp, id)
+	var i Chirp
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Body,
+		&i.UserID,
+	)
+	return i, err
+}
