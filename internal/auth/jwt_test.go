d7825f5577f2ec151105dae6f77ed451ff5e8fa4
arturogood17 <arturo_good17@hotmail.com>
2025-04-26 15:50:06 +0200

    Tests for MakeJWT  and ValidateJWT

---
+++ internal/auth/jwt_test.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func TestJWT(t *testing.T) {
+	tokenSecret := "This is a test"
+	userID := uuid.New()
+	token, _ := MakeJWT(userID, tokenSecret, 5*time.Minute)
+	tests := []struct {
+		input   string
+		tokenS  string
+		wantErr bool
+	}{
+		{
+			input:   token,
+			tokenS:  tokenSecret,
+			wantErr: false,
+		},
+		{
+			input:   token,
+			tokenS:  "123456",
+			wantErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		_, err := ValidateJWT(test.input, test.tokenS)
+		if (err != nil) != test.wantErr {
+			t.Errorf("Error: Want %v, got: %v", test.wantErr, err)
+			return
+		}
+	}
+}
