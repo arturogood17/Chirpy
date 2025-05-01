21a420e9cc2f6057c5e833adfea90c2a4c49e4c9
arturogood17 <arturo_good17@hotmail.com>
2025-05-02 00:46:06 +0200

    Added auth to polka webhook. If API sent is not the same as store, not valid

--- apiWebhookCode.go
+++ apiWebhookCode.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/arturogood17/Chirpy/internal/auth"
 	"github.com/google/uuid"
 )
 
@@ -18,6 +19,15 @@ func (a *apiConfig) WebHook(w http.ResponseWriter, req *http.Request) {
 			UserID uuid.UUID `json:"user_id"`
 		} `json:"data"`
 	}
+	authPolka, err := auth.GetAPIKey(req.Header)
+	if err != nil {
+		respondErrorWriter(w, http.StatusUnauthorized, "Error getting API key", err)
+		return
+	}
+	if authPolka != a.POLKAKEY {
+		respondErrorWriter(w, http.StatusUnauthorized, "Not authorized to do this", err)
+		return
+	}
 	var p parameters
 	decoder := json.NewDecoder(req.Body)
 	if err := decoder.Decode(&p); err != nil {
@@ -28,7 +38,7 @@ func (a *apiConfig) WebHook(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
-	_, err := a.Queries.ChirpyRed(req.Context(), p.Data.UserID)
+	_, err = a.Queries.ChirpyRed(req.Context(), p.Data.UserID)
 	if err != nil {
 		respondErrorWriter(w, http.StatusNotFound, "Error upgrading user to red", err)
 		return
